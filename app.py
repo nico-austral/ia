94e80a50cceaf236c1c1073e247b3259e472855e
GuidoCorradi1 <gcorradi@mail.austral.edu.ar>
2024-06-22 17:24:29 -0300

    Codigo 22

--- app.py
+++ app.py
@@ -30,8 +30,9 @@ def classify_waste(img):
     data[0] = normalized_image_array
     prediction = model.predict(data)
     index = np.argmax(prediction)
+    confidence = prediction[0][index]
     class_name = class_names[index].strip()
-    return class_name
+    return class_name, confidence
 
 # Aplicación Streamlit
 st.set_page_config(page_title="Clasificador de Residuos", page_icon=":recycle:", layout="centered")
@@ -39,7 +40,6 @@ st.set_page_config(page_title="Clasificador de Residuos", page_icon=":recycle:",
 # Agregar CSS personalizado
 st.markdown("""
     <style>
-    
     .stApp {
         background-color: #ffffff;
         padding-left: 50px;
@@ -69,67 +69,59 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 st.header("Boti recicla :robot_face:", divider='rainbow')
-
 st.header("Clasifica tu residuo y descubre dónde puedes tirarlo :recycle: :round_pushpin:", divider='rainbow')
 
 # Cargar imagen
-st.subheader('Subí una foto de tu resiudo')
+st.subheader('Subí una foto de tu residuo')
 input_img = st.file_uploader("", type=['jpg', 'png', 'jpeg'])
 
 if input_img:
     col1, col2 = st.columns([1, 2])
-    
     with col1:
         st.image(input_img, caption='Tu residuo', use_column_width=True)
-        
     with col2:
         st.subheader("Resultado de la clasificación")
         image_file = Image.open(input_img)
-        label = classify_waste(image_file)
-        labels_dict = {
-            "0 trash": "basura",
-            "1 battery": "batería / electrónico",
-            "2 metal": "metal",
-            "3 paper": "papel",
-            "4 glass": "vidrio",
-            "5 plastic": "plástico",
-            "6 cardboard": "cartón",
-            "7 biological": "biológico"
-        }
-        st.success(f"Tu residuo es **{labels_dict.get(label, 'No clasificado')}**")
+        label, confidence = classify_waste(image_file)
+        st.text(f"Porcentaje de confianza: {confidence*100:.2f}%")  # Mostrar en la interfaz de Streamlit
+        print(f"Porcentaje de confianza: {confidence*100:.2f}%")  # Mostrar en la consola
+        if confidence < 0.8:  # Umbral de confianza ajustable
+            st.warning("La clasificación no es segura. Por favor, sube otra foto.")
+        else:
+            labels_dict = {
+                "0 trash": "basura",
+                "1 battery": "batería / electrónico",
+                "2 metal": "metal",
+                "3 paper": "papel",
+                "4 glass": "vidrio",
+                "5 plastic": "plástico",
+                "6 cardboard": "cartón",
+                "7 biological": "biológico"
+            }
+            st.success(f"Tu residuo es **{labels_dict.get(label, 'No clasificado')}**")
 
-# Cargar el archivo de puntos verdes y las comunas
+# Cargar el archivo de puntos verdes y los barrios
 excel_file_path = os.path.join(script_dir, "puntos-verdes.xlsx")
 df = pd.read_excel(excel_file_path)
 df['Coordinates'] = df['WKT'].apply(lambda wkt: tuple(map(float, re.findall(r'[-+]?\d*\.\d+|\d+', wkt)[::-1])))
 
-# Mapear nombres de comunas a números para mostrar en el menú desplegable
-comunas_map = {
-    "Comuna 1": 1,
-    "Comuna 2": 2,
-    "Comuna 3": 3,
-    "Comuna 4": 4,
-    "Comuna 5": 5,
-    "Comuna 6": 6,
-    "Comuna 7": 7,
-    "Comuna 8": 8,
-    "Comuna 9": 9,
-    "Comuna 10": 10,
-    "Comuna 11": 11,
-    "Comuna 12": 12,
-    "Comuna 13": 13,
-    "Comuna 14": 14,
-    "Comuna 15": 15
-}
+# Filtrar por barrio usando la columna barrio
+df = df.drop_duplicates(subset=['Coordinates'])
 
-# Crear menú desplegable para seleccionar comuna
-st.subheader("Selecciona tu comuna")
-comuna_nombre = st.selectbox("",list(comunas_map.keys()))
-if comuna_nombre:
-    comuna_numero = comunas_map[comuna_nombre]
-    filtered_df = df[df['comuna'] == f'COMUNA {comuna_numero}']
+st.subheader("Selecciona tu barrio")
+barrios_disponibles = df['barrio'].unique()  # Asegúrate de que 'barrio' es la columna que contiene los nombres de los barrios
+barrio_nombre = st.selectbox("", barrios_disponibles)
+
+# Filtrar por tipo de residuo
+
+residuos_disponibles = df['materiales'].str.split(',').apply(lambda x: [item.strip() for item in x]).explode().unique()
+
+residuo_tipo = st.selectbox("Selecciona el tipo de residuo", residuos_disponibles)
+
+if barrio_nombre and residuo_tipo:
+    filtered_df = df[(df['barrio'].str.contains(barrio_nombre, case=False)) & (df['materiales'].str.contains(residuo_tipo, case=False))]
     if filtered_df.empty:
-        st.warning("No hay puntos de reciclaje en esta comuna")
+        st.warning("No hay puntos de reciclaje para este tipo de residuo en este barrio")
     else:
         st.subheader("Mapa de sitios de reciclaje")
         map_center = filtered_df['Coordinates'].apply(pd.Series).mean().tolist()
@@ -138,14 +130,17 @@ if comuna_nombre:
             folium.Marker(row['Coordinates'], popup=f"<b>Materiales:</b> {row['materiales']}").add_to(m)
         st_folium(m, width=700, height=500)
 
-st.balloons()
-
 # Sección de suscripción al newsletter
 st.header("Suscríbete a nuestro newsletter")
 st.write("Enterate de las últimas novedades e iniciativas sobre reciclaje y sustentabilidad en la Ciudad de Buenos Aires")
 email = st.text_input("Ingresa tu correo electrónico")
+
+# Función para validar correo electrónico
+def is_valid_email(email):
+    return re.match(r"[^@]+@[^@]+\.[^@]+", email)
+
 if st.button("Suscribirse"):
-    if email:
+    if email and is_valid_email(email):
         # Guardar el email en un archivo de texto
         subscribers_file_path = os.path.join(script_dir, "subscribers.txt")
         with open(subscribers_file_path, "a") as f:
